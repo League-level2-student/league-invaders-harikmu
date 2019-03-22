f8b3d3e930746aa419702086862243b9b4e1b3ad
harikmu <league@wts-im-17.attlocal.net>
2019-03-22 16:56:45 -0700

    pt 2 step 3

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,16 @@
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
-
+	public static void main(String[] args) throws Exception {
+		LeagueInvaders leagueinvaders = new LeagueInvaders();
+		leagueinvaders.setup();
+	}
+	JFrame f = new JFrame();
+	final int width = 500;
+	final int height = 800;
+	private void setup() {
+		f.setVisible(true);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setSize(width,height);
+	}
 }
